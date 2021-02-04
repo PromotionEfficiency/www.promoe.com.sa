87e08d9a27d51b138dfb78f7bc06fa588979a984
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2021-02-04 23:40:55 +0900

    V2

---
+++ _nuxt/static/1612449471/Jobs/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/Jobs", {data:[{}],fetch:[],mutations:[]});
---
+++ _nuxt/static/1612449471/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002FAbout","\u002FJobs","\u002FRequest","\u002F","\u002FContact"]})
